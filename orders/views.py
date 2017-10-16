e1612cf7a3aef7e14ac6841bacf7f290c6d9260e
Mwai Karimi <mwaikarimi@Mwais-MacBook-Pro.local>
2017-10-16 12:55:19 +0100

    update create-order view

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -19,7 +20,15 @@ def create_order(request):
 
             # launch async task
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+
+            # return render(request, 'orders/order/created.html', {'order': order})
+
+            # set the order in the session
+            request.session['order_id'] = order.id
+
+            # redirect to payment
+            return redirect(reverse('payment:process'))
+
     else:
             form = OrderCreateForm
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
